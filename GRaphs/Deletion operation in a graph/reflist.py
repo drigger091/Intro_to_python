affc3d4974210e1a5773d712619732a30f6c34c4
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-01-20 21:03:21 +0530

    Deletion operation

---
+++ GRaphs/Deletion operation in a graph/reflist.py
@@ -0,0 +1,75 @@
+#this ref is created in accordance to adjacency list
+
+
+#creating the dictionary to represent the graoh instead of a matrix
+
+graph = {}
+
+def add_nodes(v):
+    if v in graph.keys():
+        print(v,"is present in graph")
+    else:
+        graph[v] = []
+
+def add_edgeUWUD(v1,v2): #undirected and unweighted graph
+    if v1 not in graph:
+        print(v1,"is not present in the graph")
+    elif v2 not in graph:
+        print(v2,"is not present in the graph")
+    else:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+
+#here it is a nested list the first value will be the value and the cost
+def add_edgeWUD(v1,v2,cost): #undirected and weighted graph
+    if v1 not in graph:
+        print(v1,"is not present in the graph")
+    elif v2 not in graph:
+        print(v2,"is not present in the graph")
+    else:
+        list1 = [v2,cost]
+        list2 = [v1,cost]
+        graph[v1].append(list1)
+        graph[v2].append(list2)
+
+def add_edgeWD(v1,v2,cost): # weighted and directed graph
+    if v1 not in graph:
+        print(v1,"is not present in the Graph")
+    elif v2 not in graph:
+        print(v2,"is not present in th graph")
+    else:
+        list1 = [v2,cost]
+        graph[v1].append(list1)
+
+def add_edgeUWD(v1,v2): #un weighted but directed graph
+    if v1 not in graph:
+        print(v1,"is not present in th graph")
+    elif v2 not in graph:
+        print(v2 , "is not present in the graph")
+    else:
+        graph[v1].append(v2)
+
+def delete_node(v):
+    if v not in graph:
+        print(v, "is not present in the graph")
+    else:
+        
+
+
+
+
+
+add_nodes("A")
+
+add_nodes("B")
+add_nodes("C")
+add_nodes("D")
+add_nodes("E")
+add_edgeUWUD("D","B")
+add_edgeWUD("A","B",10)
+add_edgeWUD("A","C",5)
+add_edgeWD("E","D",20)
+add_edgeUWD("E","A")
+print("After adding nodes")
+print(graph)
---
+++ GRaphs/Deletion operation in a graph/refmatrix.py
@@ -0,0 +1,108 @@
+
+#this is a reference to the adjacny maxtrix
+
+
+nodes = []  # keeping the count if the nodes added in the graph
+graph = []     #used to determin the adjaceny matrix for edges and nodes
+
+node_count = 0
+
+def add_node(v):
+
+    global node_count  #updating a global variable
+    if v in nodes:
+        print("The Node is already present in the graph")
+    else:
+        node_count = node_count +1
+        nodes.append(v)
+        for n in graph:
+            n.append(0)
+        temp = []
+
+        for i in range(node_count):
+            temp.append(0)
+        graph.append(temp)
+
+
+#before performing we check whther v1 and v2 in nodes
+def add_edge(v1,v2): #undirected graph and inweighted graphso no weights
+    if v1 not in nodes:
+        print(v1,"is not present in the nodes")
+    elif v2  not in nodes:
+        print(v2,"is not present in the nodes")
+    else:
+         index1 = nodes.index(v1) #getting the index from node list and change accoording in the graph maxtrix
+         index2 = nodes.index(v2)
+         graph[index1][index2] = 1
+         graph[index2][index1] = 1
+
+def add_edgeWeighted(v1 ,v2 , cost):#undirected weighted graph
+    if v1 not in nodes:
+        print(v1,"is not present in the nodes")
+    elif v2  not in nodes:
+        print(v2,"is not present in the nodes")
+    else:
+         index1 = nodes.index(v1) #getting the index from node list and change accoording in the graph maxtrix
+         index2 = nodes.index(v2)
+         graph[index1][index2] = cost
+         graph[index2][index1] = cost
+
+def add_edgeDW(v1,v2,cost): #here the edge is conected from v1 to v2 only change in v1 row and v2 column
+    if v1 not in nodes:
+        print(v1,"not present in nodes")
+    elif v2 not in nodes:
+        print(v2,"is not present in the nodes")
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2]= cost
+
+        #incase of directed inweighted we only add 1 instead of cost in the maxtrix
+
+def delete_nodeUDUW(v): #undirected unweighted graph
+    
+    if v not in nodes:
+        print(v,"is not present in the graph")
+    else:
+        index1 = nodes.index(v)
+        node_count = node_count -1
+        nodes.remove(v)
+        graph.pop(index1)
+        for i in graph:
+            i.pop(index1)
+
+
+
+def print_graph():
+
+    for i in range(node_count):
+
+        for j in range(node_count):
+            print(format(graph[i][j],"<3"), end =" ")
+        print ()
+
+
+
+
+#print("Before adding nodes:")
+#print(nodes)
+#print(graph)
+add_node("A")
+add_node("B")
+add_node("C")
+add_node("D")
+print("After adding nodes:")
+add_edge("A","B") # adding edge between A and B
+add_edgeWeighted("A","B",10)
+add_edgeWeighted("A","D",5)
+add_edgeDW("C","D",4)
+
+print(nodes)
+#print(graph)
+print_graph()
+delete_nodeUDUW("D")
+print(node_count)
+
+# it is all zero as there are no edges added till yet so no connection
+
+
