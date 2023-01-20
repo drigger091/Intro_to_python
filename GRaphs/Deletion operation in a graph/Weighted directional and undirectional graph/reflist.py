30249aa495e05ef7645370e87b75c7de44884eb9
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-01-21 00:03:38 +0530

    Delete edge operation for Weighted graphs (directed and undirected)

--- GRaphs/Deletion operation in a graph/Weighted directional and undirectional graph/reflist.py
+++ GRaphs/Deletion operation in a graph/Weighted directional and undirectional graph/reflist.py
@@ -50,7 +50,33 @@ def delete_node(v): # works for  weighted directed and undirected graph
                     list1.remove(j)
                     break
         
+def delete_edge(v1,v2,cost): #this one is for weighted undirected graph
+    if v1 not in graph:
+        print(v1,"is not present in the graph")
+    elif v2 not in graph:
+        print(v2,"is not present in the graph")
+    else:
+        temp1 = [v1,cost]
+        temp2 = [v2,cost]
+        if temp2 in graph[v1]:          # we are checking whther temp is present in the value of graph dictionary with key v1
+            graph[v1].remove(temp2)      # we are deleting the entire list from the value of the particlar key and vice versa
+            graph[v2].remove(temp1)
+        else:
+            print("There is no edge between ",v1, "and",v2)
 
+def delete_edgeD(v1,v2,cost): #this one is for weighted undirected graph
+    if v1 not in graph:
+        print(v1,"is not present in the graph")
+    elif v2 not in graph:
+        print(v2,"is not present in the graph")
+    else:
+        temp1 = [v1,cost]
+        temp2 = [v2,cost]
+        if temp2 in graph[v1]:          # we are checking whther temp is present in the value of graph dictionary with key v1
+            graph[v1].remove(temp2)      
+            
+        else:
+            print("There is no edge between ",v1, "and",v2)
                 
 
 
@@ -73,10 +99,12 @@ add_nodes("F")
 add_edgeWUD("A","B",10) # both A and B are connected with each other via weight
 add_edgeWUD("A","C",5)  # both way connected
 add_edgeWD("E","D",20)  # only E connected with D with the mentioned weight
-add_edgeWD("D","F",50)
+add_edgeWD("D","F",50) # connects one way from D to f
 add_edgeWUD("C","F",25)
 
-delete_node("F") # deleting the specified node
+#delete_node("F") # deleting the specified node
+delete_edge("A","C",5)
+delete_edgeD("E","D",20)
 
 print("After adding nodes")
 print(graph)
