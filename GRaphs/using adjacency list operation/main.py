dcc5efaf29736a48ca338b6321557fcbd3c373bc
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-01-20 14:53:23 +0530

    Updating various add edges functions

--- GRaphs/using adjacency list operation/main.py
+++ GRaphs/using adjacency list operation/main.py
@@ -30,11 +30,32 @@ def add_edgeWUD(v1,v2,cost): #undirected and weighted graph
         graph[v1].append(list1)
         graph[v2].append(list2)
 
+def add_edgeWD(v1,v2,cost): # weighted and directed graph
+    if v1 not in graph:
+        print(v1,"is not present in the Graph")
+    elif v2 not in graph:
+        print(v2,"is not present in th graph")
+    else:
+        list1 = [v2,cost]
+        graph[v1].append(list1)
+
+def add_edgeUWD(v1,v2): #un weighted but directed graph
+    if v1 not in graph:
+        print(v1,"is not present in th graph")
+    elif v2 not in graph:
+        print(v2 , "is not present in the graph")
+    else:
+        graph[v1].append(v2)
+
 add_nodes("A")
 add_nodes("A")
 add_nodes("B")
 add_nodes("C")
-#add_edgeUWUD("A","B")
+add_nodes("D")
+add_nodes("E")
+add_edgeUWUD("D","B")
 add_edgeWUD("A","B",10)
 add_edgeWUD("A","C",5)
+add_edgeWD("E","D",20)
+add_edgeUWD("E","A")
 print(graph)
