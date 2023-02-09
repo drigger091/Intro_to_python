31c5ad2e94bdd6ea182be925d37587db061db36d
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-02-09 23:56:22 +0530

    update dbapp file(adding try and except)

--- Gracious error handling/Error handling in flask/dbapp.py
+++ Gracious error handling/Error handling in flask/dbapp.py
@@ -1,4 +1,17 @@
+#types of exceptions we may find is that we cannot find the blog 
+# we may come across a blog that is not public
+
+
+
+
 import sqlite3
+
+class Notfounderror(Exception):
+    pass
+
+
+class Notauthorizederror(Exception):
+    pass
 
 def blog_lst_conv_to_json(item):
 
@@ -14,6 +27,7 @@ def blog_lst_conv_to_json(item):
 def fetch_blogs():
 
     #connect to the database
+    
 
     con = sqlite3.connect("application.db")
 
@@ -29,23 +43,41 @@ def fetch_blogs():
     result = list(map(blog_lst_conv_to_json,cur.fetchall()))
 
     #close the database
-    cur.close()
+    con.close()
 
     return result
+    
+
+
 
 
 def fetch_blog(id:str):
 
     #connect the database
-
-    con = sqlite3.connect('application.db')
-    cur = con.cursor()
+    try:
+        con = sqlite3.connect('application.db')
+        cur = con.cursor()
 
     #query to fetch the data
 
-    cur.execute(f"SELECT * FROM blogs where id = '{id}'")
-    result = cur.fetchone()
+        cur.execute(f"SELECT * FROM blogs where id = '{id}'")
+        result = cur.fetchone()
 
-    data = blog_lst_conv_to_json(result)
+        #if the result from the request retuns none , we are handling that exception as well
+        if result is None:
+            raise Notfounderror(f"Unable to find blog with id {id}.")
 
-    return data
+        data = blog_lst_conv_to_json(result)
+        #now we have to check wther the return bloc is allowed to be viewed in public or not
+        if not data['public']:
+            raise Notauthorizederror(f"You are not authorized to access the blog with id {id}.")
+
+        return data
+
+    except sqlite3.OperationalError as ex1:
+        print(ex1)
+
+        raise Notfounderror(f"Unable to fin blog with id {id}.")
+    finally:
+        #close the database
+        con.close()
